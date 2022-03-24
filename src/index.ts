10422a9ce0d1f0a1247029e084a68dbf93216926
Samuel Massé <samuelmasse4@gmail.com>
2022-03-24 14:24:54 -0400

    working

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
 import { Client, TextChannel } from 'discord.js'
 import config from './config.json'
 import { MessagingClient } from '@botpress/messaging-client'
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+const messaging = new MessagingClient({ ...config.messaging })
+messaging.setup(app)
+
+const port = 3124
+app.listen(port)
+console.log(`listening on port ${port}`)
 
 const discord = new Client({
   intents: ['GUILDS', 'DIRECT_MESSAGES', 'GUILD_MESSAGES'],
   partials: ['MESSAGE', 'CHANNEL']
 })
-const messaging = new MessagingClient({ ...config.messaging })
 
 discord.once('ready', () => {
-  console.log('Ready!')
+  console.log(`discord logged in as ${discord.user!.username} (${discord.user!.id})`)
 })
 
 discord.on('messageCreate', async (message) => {
@@ -19,13 +29,14 @@ discord.on('messageCreate', async (message) => {
 
   const conversationId = await messaging.mapEndpoint({
     channel: 'discord',
-    identity: '*',
+    identity: discord.user!.id,
     sender: message.author.id,
     thread: message.channel.id
   })
   const { userId } = (await messaging.getConversation(conversationId))!
 
-  await messaging.createMessage(conversationId, userId, { type: 'text', text: message.content })
+  const sent = await messaging.createMessage(conversationId, userId, { type: 'text', text: message.content })
+  console.log('sent', sent)
 })
 
 messaging.on('message', async ({ message }) => {
@@ -37,6 +48,7 @@ messaging.on('message', async ({ message }) => {
 
   const channel = discord.channels.cache.get(endpoint.thread) as TextChannel
   await channel.send(message.payload.text)
+  console.log('received', message)
 })
 
 discord.login(config.discord.token)
